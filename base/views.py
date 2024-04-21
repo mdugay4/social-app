579a8ee59f89e3d61b0fa7b35188472874c4e581
Mark Dugay <mdugay4@gmail.com>
2024-04-20 23:38:12 -0700

    home & room templates

--- base/views.py
+++ base/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-from django.http import HttpResponse
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, id):
+    room = None
+    for i in rooms:
+        if i['id'] == int(id):
+            room = i
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
