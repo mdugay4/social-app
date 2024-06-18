da995fe03b4276953788e741030cf35850050e85
Mark Dugay <mdugay4@gmail.com>
2024-06-17 18:39:23 -0700

    Build config

--- build_files.sh
+++ build_files.sh
@@ -7,10 +7,10 @@ python3 -m venv venv
 source venv/bin/activate
 
 # install all deps in the venv
-# python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # collect static files using the Python interpreter from venv
-# python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "BUILD END"
 
